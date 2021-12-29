81b7bd3567daf34eed2f3e8e83ce51c0cdc488cf
etidahouse <91779963+etidahouse@users.noreply.github.com>
2021-12-29 10:13:56 +0100

    Remove swagger annotation

--- src/main/java/app/RestAPI.java
+++ src/main/java/app/RestAPI.java
@@ -7,16 +7,6 @@ import javax.ws.rs.ApplicationPath;
 
 import org.glassfish.jersey.server.ResourceConfig;
 
-import business.JWTHelper;
-import io.swagger.v3.jaxrs2.integration.JaxrsOpenApiContextBuilder;
-import io.swagger.v3.jaxrs2.integration.resources.OpenApiResource;
-import io.swagger.v3.oas.integration.OpenApiConfigurationException;
-import io.swagger.v3.oas.integration.SwaggerConfiguration;
-import io.swagger.v3.oas.models.OpenAPI;
-import io.swagger.v3.oas.models.info.Contact;
-import io.swagger.v3.oas.models.info.Info;
-import io.swagger.v3.oas.models.security.SecurityScheme;
-import io.swagger.v3.oas.models.servers.Server;
 import lombok.extern.log4j.Log4j2;
 
 @ApplicationPath("api")
