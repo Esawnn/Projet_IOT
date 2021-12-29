4f1338868aee928a19b910e357d6ddff366a9c79
etidahouse <91779963+etidahouse@users.noreply.github.com>
2021-12-29 10:12:10 +0100

    remove swagger code

--- src/main/java/app/RestAPI.java
+++ src/main/java/app/RestAPI.java
@@ -25,44 +25,6 @@ public class RestAPI extends ResourceConfig {
 
 	public RestAPI() {
 		packages("resource");
-
-
-		log.debug("Lancement du ressource config de l'application.");
-		log.debug("Création de l'instance JWTTokenHelper.");
-		JWTHelper.getInstance();
-
-		log.debug("Création de l'OpenAPI (Swagger).");
-		OpenAPI openApi = new OpenAPI();
-		Info info = new Info().title("Personne Open API").description("Documentation de l'API Personne.")
-				.contact(new Contact().email("personne@mycompany.com")).version("0.0.1");
-		openApi.info(info);
-		openApi.schemaRequirement("bearer-auth",
-				new SecurityScheme().scheme("BEARER").description("JWT authentication with bearer token.")
-						.name("Authorization").type(SecurityScheme.Type.APIKEY).bearerFormat("BEARER [token]")
-						.in(SecurityScheme.In.HEADER));
-		log.debug("Open API info. " + openApi.toString());
-		Server server = new Server();
-		server.setUrl("http://localhost:8080/PersonneProject/");
-		openApi.addServersItem(server);
-		log.debug("Server URL : " + server.getUrl());
-		SwaggerConfiguration oasConfig = new SwaggerConfiguration()
-				.resourcePackages(
-						Stream.of("resource").collect(Collectors.toSet()))
-				.prettyPrint(true).cacheTTL(0L).scannerClass("io.swagger.v3.jaxrs2.integration.JaxrsAnnotationScanner")
-				.readAllResources(false).openAPI(openApi);
-		log.debug("Swagger Configuration : " + oasConfig.getResourcePackages().toString());
-		OpenApiResource openApiResource = new OpenApiResource();
-		openApiResource.setOpenApiConfiguration(oasConfig);
-		register(openApiResource);
-		try {
-			new JaxrsOpenApiContextBuilder().application(this.getApplication()).openApiConfiguration(oasConfig)
-					.buildContext(true);
-		} catch (OpenApiConfigurationException e) {
-			log.error("Error on swagger config.");
-			throw new RuntimeException(e.getMessage(), e);
-		}
-		log.debug("Fin de la génération de l'OpenAPI descriptor.");
-
 	}
 
 }
