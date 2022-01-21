8f61d157496504389b9767357e92bf360995e571
dodo120 <dodocazabat@gmail.com>
2022-01-21 12:12:48 +0100

    tab publisher

---
+++ src/main/java/entity/TabPublisher.java
@@ -0,0 +1,15 @@
+package entity;
+
+public class TabPublisher {
+	
+	private String[] publishers;
+
+	public String[] getPublishers() {
+		return publishers;
+	}
+
+	public void setPublishers(String[] publishers) {
+		this.publishers = publishers;
+	}
+
+}
